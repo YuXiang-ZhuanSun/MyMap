db088e601f07003156c570475a65e5560375b538
ZhuansunYuxiang <183010115@qq.com>
2021-04-22 19:02:49 +0800

    添加建树时长检测（验证远优于BST,  BST:22s, RBT:0.2s）

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@
 
 int main() {
 
+	clock_t begin = clock();
+	
+		
 	MyMap<int, int> * m = new MyMap<int, int>();
-	for (int i = 1; i <= 1000; i++) {
+	for (int i = 1; i <= 100000; i++) {
 		m->insert_MyPair(i, 1000 - i);
 	}
-
+	clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "建树耗时：" <<time_spent<< std::endl;
+    
 	std::cout << "查找操作：" << std::endl;
 	MyPair<int, int> * p = m->find(6);
-	p->print();
+	if(p){
+		p->print();
+	}
 
 	std::cout << "中序遍历：" << std::endl;
 	m->in_order();
